788adf2fbc66d61409fc7e32a4f1c2bce3cdc241
ABA Games <cs8k-cyu@asahi-net.or.jp>
2019-03-22 10:00:15 +0900

    Handle player

--- src/actor.ts
+++ src/actor.ts
@@ -6,6 +6,7 @@ import { wrap } from "./util/math";
 export class Actor extends sga.Actor {
   pos = new Vector();
   prevPos = new Vector();
+  intPos = new Vector();
   char = "";
   options = undefined as terminal.CharOptions;
 
@@ -14,9 +15,29 @@ export class Actor extends sga.Actor {
     super.update();
     this.pos.x = wrap(this.pos.x, 0, terminal.size);
     this.pos.y = wrap(this.pos.y, 0, terminal.size);
+    this.intPos.set(Math.floor(this.pos.x), Math.floor(this.pos.y));
+    if (this.options.color === "c") {
+      this.handlePlayer();
+    }
+  }
+
+  handlePlayer() {
+    sga.pool.get().forEach((a: Actor) => {
+      if (a.options.color !== "r") {
+        return;
+      }
+      if (this.testCollision(a)) {
+        this.remove();
+        a.remove();
+      }
+    });
+  }
+
+  testCollision(other: Actor) {
+    return this.intPos.x === other.intPos.x && this.intPos.y === other.intPos.y;
   }
 
   draw() {
-    terminal.setCharAt(this.pos.x, this.pos.y, this.char, this.options);
+    terminal.setCharAt(this.intPos.x, this.intPos.y, this.char, this.options);
   }
 }
